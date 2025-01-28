7f3d0f8d53d18ccf4a6b4efaeb466c5faef6168f
AbelrahmanEldesouky <abdelrhman.aldesoky@outlook.com>
2025-01-28 22:25:04 +0200

    solve: update time format

--- src/format.cpp
+++ src/format.cpp
@@ -1,6 +1,8 @@
-#include <string>
-
 #include "format.h"
+
+#include <iomanip>
+#include <sstream>
+#include <string>
 
 using std::string;
 
@@ -8,4 +10,17 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+std::string Format::ElapsedTime(long seconds) {
+  long HH = seconds / 3600;
+  seconds %= 3600;
+  long MM = seconds / 60;
+  seconds %= 60;
+
+  // Use a string stream to format the output with leading zeros
+  std::ostringstream elapsed_time;
+  elapsed_time << std::setw(2) << std::setfill('0') << HH << ":" << std::setw(2)
+               << std::setfill('0') << MM << ":" << std::setw(2)
+               << std::setfill('0') << seconds;
+
+  return elapsed_time.str();
+}
