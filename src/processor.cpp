d8e9f5c83042ffa4628056b9c6465f9c4bbce7a6
AbelrahmanEldesouky <abdelrhman.aldesoky@outlook.com>
2025-01-28 22:53:42 +0200

    solve: update processor

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,10 @@
 #include "processor.h"
 
+#include "linux_parser.h"
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+  auto total_t = LinuxParser::Jiffies();
+  auto active_t = LinuxParser::ActiveJiffies();
+
+  return float(active_t) / float(total_t);
+}
